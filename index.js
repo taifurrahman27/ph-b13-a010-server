edf34027ed7bb38cc6c39e34b629914642f2a209
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-07-25 15:37:10 +0600

    writers data loaded from db

--- index.js
+++ index.js
@@ -20,6 +20,43 @@ await connectDB();
 
 app.use("/ebooks", ebookRoutes);
 
+app.get("/writers", async (req, res) => {
+    try {
+        const db = await connectDB();
+
+        const ebooks = await db.collection("ebooks").find().toArray();
+
+        const writersMap = {};
+
+        ebooks.forEach((ebook) => {
+            const writer = ebook.writer;
+
+            if (!writer) return;
+
+            if (!writersMap[writer.id]) {
+                writersMap[writer.id] = {
+                    id: writer.id,
+                    name: writer.name,
+                    email: writer.email,
+                    photo: writer.photo,
+                    totalBooks: 0,
+                    totalSales: 0,
+                };
+            }
+
+            writersMap[writer.id].totalBooks += 1;
+            writersMap[writer.id].totalSales += ebook.totalSales || 0;
+        });
+
+        res.json(Object.values(writersMap));
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Failed to load writers",
+        });
+    }
+});
+
 
 
 
